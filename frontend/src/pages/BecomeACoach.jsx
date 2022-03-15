8b0e8c8233b5ddf024d280c8d5dede2058cba73d
devharp <hsingh.161297@gmail.com>
2022-03-15 08:39:50 +0530

    BecomeACoach Block3 completed

--- frontend/src/pages/BecomeACoach.jsx
+++ frontend/src/pages/BecomeACoach.jsx
@@ -13,6 +13,8 @@ import { TextField, Button } from '@mui/material/';
 
 
 import img00 from '../assets/becomeacoach/00.svg';
+import img01 from '../assets/becomeacoach/01.svg';
+import img02 from '../assets/becomeacoach/02.svg';
 
 
 function BecomeACoach(props) {
@@ -97,15 +99,23 @@ function Desktop(props) {
     function Block2() {
         function BackgroundImages() {
             return (
-                <div className="position-absolute">
-                    Desktop View1gmkdfk
+                <div className="position-absolute" style={{right: '0'}}>
+                    <img className='position-relative' src={img01} style={{top: `calc(198/1080 * ${props.bheight})`, height: `calc(760/1080 * ${props.bheight})`, width: `calc(760/1920 * 100vw * 1613/760)`}} alt="" />
                 </div>
             );
         }
         function FrontContent() {
             return (
-                <div className="position-absolute">
-                    Desktop View0
+                <div className="pt-5 position-absolute d-flex flex-column align-items-center" style={{left: `calc(763/1920 * 100vw)`}}>
+                    <div className='my-5' style={{ fontWeight: '500', fontSize: `calc(36/1080 * ${props.bheight})`, letterSpacing: `calc(3.6/1080 * ${props.bheight})`}}>We Achieve this by Offering:</div>
+                    <div className='d-flex flex-column align-items-center' style={{fontSize: `calc(28/1080 * ${props.bheight})`}}>
+                        <div style={{ whiteSpace: 'nowrap' }}>With Go Girl, You have a lot of flexibility in terms of your</div>
+                        <div style={{ whiteSpace: 'nowrap' }}>work schedule and timing. We let you communicate</div>
+                        <div style={{ whiteSpace: 'nowrap' }}>directly with your clients and fix the timings that work for</div>
+                        <div style={{ whiteSpace: 'nowrap' }}>you both. In addition, you won't face any peer pressure at</div>
+                        <div style={{ whiteSpace: 'nowrap' }}>Go Girl so that you will have a fun and self-motivated work</div>
+                        <div style={{ whiteSpace: 'nowrap' }}>environment.</div>
+                    </div>
                 </div>
             );
         }
@@ -120,14 +130,32 @@ function Desktop(props) {
         function BackgroundImages() {
             return (
                 <div className="position-absolute">
-                    Desktop View1gmkdfk
+                    {/* Desktop View1gmkdfk */}
                 </div>
             );
         }
         function FrontContent() {
             return (
-                <div className="position-absolute">
-                    Desktop View0
+                <div className="position-absolute d-flex flex-row align-items-center" style={{ width: '100%', height: props.bheight }}>
+                    <img src={img02} style={{ width: `calc(985/1920 * 100vw)`, height: `calc(560/1080 * ${props.bheight})` }} alt="" />
+                    <div className="d-flex flex-column align-items-center flex-grow-1">
+                        <div className="mb-5" style={{
+                            fontSize: `calc(36/1080 * ${props.bheight})`, 
+                            letterSpacing: `calc(3.6/1080 * ${props.bheight})`,
+                            fontWeight: '500'    
+                        }}>Exposure to a Range of People</div>
+                        <div className="d-flex flex-column align-items-center" style={{ fontSize: `calc(28/1080 * ${props.bheight})` }}>
+                            <div>You can get the freedom to explore</div>
+                            <div>yourself as an independent coach.</div>
+                            <div>Also, you&apos;ll get the opportunity to work</div>
+                            <div>along with industry experts,</div>
+                            <div>nutritionists, and skilled trainers. You</div>
+                            <div>can always avail of guidance and</div>
+                            <div>support from experienced trainers.</div>
+                            <div>Thus Go Girl works on you to make</div>
+                            <div>you more proficient.</div>
+                        </div>
+                    </div>
                 </div>
             );
         }
@@ -163,13 +191,13 @@ function Desktop(props) {
 
     return (
         <div className={props.className} style={props.style}>
-            <div className="bg-danger0" style={{ height: props.bheight }}>
+            <div className="" style={{ height: props.bheight }}>
                 <Block1 />
             </div>
-            <div className="bg-danger1" style={{ height: props.bheight }}>
+            <div className="" style={{ height: props.bheight, backgroundColor: '#FBE551' }}>
                 <Block2 />
             </div>
-            <div className="bg-danger" style={{ height: props.bheight }}>
+            <div className="bg-info" style={{ height: props.bheight }}>
                 <Block3 />
 
             </div>
@@ -228,7 +256,7 @@ function Mobile(props) {
                                     <div>Are you ready to discover the path to becoming</div>
                                     <div>a Go Girl Coach?</div>
                                 </div>
-                            </div>                            
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -245,14 +273,17 @@ function Mobile(props) {
         function BackgroundImages() {
             return (
                 <div className="position-absolute">
-                    Desktop View1gmkdfk
+                    <img className='mt-3' src={img01} style={{height: `calc(160/640 * ${props.bheight})`, width: `calc(340/360 * 100vw)`}} alt="" />
                 </div>
             );
         }
         function FrontContent() {
             return (
-                <div className="position-absolute">
-                    Desktop View0
+                <div className="position-absolute d-flex flex-column justify-content-center" style={{width: '100vw', height: props.bheight}}>
+                    <div className='d-flex justify-content-center my-5' style={{ fontWeight: '500', fontSize: `calc(18/640 * ${props.bheight})`, letterSpacing: `calc(1.8/640 * ${props.bheight})`}}>We Achieve this by Offering:</div>
+                    <div className='d-flex justify-content-center' style={{fontSize: `calc(14/640 * ${props.bheight})`}}>
+                        <div className='' style={{textAlign: 'center', maxWidth: 'calc(320px)'}}>With Go Girl, You have a lot of flexibility in terms of your work schedule and timing. We let you communicate directly with your clients and fix the timings that work for you both. In addition, you won't face any peer pressure at Go Girl so that you will have a fun and self-motivated work environment.</div>
+                    </div>
                 </div>
             );
         }
@@ -267,14 +298,20 @@ function Mobile(props) {
         function BackgroundImages() {
             return (
                 <div className="position-absolute">
-                    Desktop View1gmkdfk
+                    {/* Desktop View1gmkdfk */}
                 </div>
             );
         }
         function FrontContent() {
             return (
-                <div className="position-absolute">
-                    Desktop View0
+                <div className="position-absolute" style={{width: '100%'}}>
+                    <div className="d-flex justify-content-center"><img style={{ width: `calc(287/360 * 100vw)`, height: `calc(264/640 * ${props.bheight})` }} src={img02} alt="" /></div>
+                    <div className="d-flex flex-column align-items-center bg-">
+                        <div className='my-4' style={{ fontSize: `calc(18/640 * ${props.bheight})`, letterSpacing: `calc(1.8/640 * ${props.bheight})`, fontWeight: '500' }}>Exposure to a Range of People</div>
+                        <div className="" style={{ fontSize: `calc(14/640 * ${props.bheight})`, maxWidth: '320px', textAlignLast: 'center', textAlign: 'justify' }}>
+                        You can get the freedom to explore yourself as an independent coach. Also, you’ll get the opportunity to work along with industry experts, nutritionists, and skilled trainers. You can always avail of guidance and support from experienced trainers. Thus Go Girl works on you to make you more proficient.
+                        </div>
+                    </div>
                 </div>
             );
         }
@@ -310,13 +347,13 @@ function Mobile(props) {
 
     return (
         <div className={props.className} style={props.style}>
-            <div className="bg-danger1" style={{ height: props.bheight }}>
+            <div className="" style={{ height: props.bheight }}>
                 <Block1 />
             </div>
-            <div className="bg-danger" style={{ height: props.bheight }}>
+            <div className="" style={{ height: props.bheight, backgroundColor: '#FBE551' }}>
                 <Block2 />
             </div>
-            <div className="bg-danger" style={{ height: props.bheight }}>
+            <div className="bg-info" style={{ height: props.bheight }}>
                 <Block3 />
 
             </div>
