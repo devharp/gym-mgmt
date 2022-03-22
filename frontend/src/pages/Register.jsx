aa7cde1f0745756930ffff23e6897bf6589a1dfa
devharp <hsingh.161297@gmail.com>
2022-03-22 06:38:52 +0530

    Fixed the missing image of the 'Register page'

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,13 +3,22 @@ import NavBar from "../components/navbar/NavBar";
 
 import { Box, Button, Checkbox, TextField, InputAdornment } from "@mui/material"
 import { AccountCircle, Email, PhoneAndroid, Lock, Facebook, Google } from '@mui/icons-material';
+
+import img00 from '../assets/register/00.png'
+import imggetstarted from '../assets/register/getstartedhere0.svg'
+
+import { bgcolor } from "@mui/system";
 function Register(props) {
     useEffect(() => {
         window.scroll(0, 0);
-    
-      
+        // document.body.style.backgroundColor = '#FBE551';
+
+
     }, [])
     function Desktop(props) {
+        const inputValuesStyle = {
+            fontSize: `calc(20/1080 * ${props.bheight})`
+        }
         return (
             <div className={props.className} style={props.style}>
                 <div className="position-absolute d-flex" >
@@ -23,28 +32,28 @@ function Register(props) {
                         <div className="mb-3" style={{ fontSize: `calc(36/1080 * ${props.bheight})`, letterSpacing: `calc(3.6/1080 * ${props.bheight})`, fontWeight: '500' }}>Register</div>
                         <TextField className="mb-3" label='Username' size="small" placeholder="Enter Username Here"
                             InputProps={{
-                                style: { fontSize: `calc(28/1080 * ${props.bheight})` },
+                                style: inputValuesStyle,
                                 startAdornment: <InputAdornment position="start"><AccountCircle /></InputAdornment>
                             }}
-                            InputLabelProps={{ style: { fontSize: `calc(28/1080 * ${props.bheight})` } }} />
+                            InputLabelProps={{ style: inputValuesStyle }} />
                         <TextField className="mb-3" label='Email' size="small" placeholder="Enter Email Here"
                             InputProps={{
-                                style: { fontSize: `calc(28/1080 * ${props.bheight})` },
+                                style: inputValuesStyle,
                                 startAdornment: <InputAdornment position="start"><Email /></InputAdornment>
                             }}
-                            InputLabelProps={{ style: { fontSize: `calc(28/1080 * ${props.bheight})` } }} />
+                            InputLabelProps={{ style: inputValuesStyle }} />
                         <TextField className="mb-3" label='Mobile' size="small" placeholder="Enter Mobile Number Here"
                             InputProps={{
-                                style: { fontSize: `calc(28/1080 * ${props.bheight})` },
+                                style: inputValuesStyle,
                                 startAdornment: <InputAdornment position="start"><PhoneAndroid /></InputAdornment>
                             }}
-                            InputLabelProps={{ style: { fontSize: `calc(28/1080 * ${props.bheight})` } }} />
+                            InputLabelProps={{ style: inputValuesStyle }} />
                         <TextField className="mb-3" label='Password' size="small" placeholder="Enter Password Here"
                             InputProps={{
-                                style: { fontSize: `calc(28/1080 * ${props.bheight})` },
+                                style: inputValuesStyle,
                                 startAdornment: <InputAdornment position="start"><Lock /></InputAdornment>
                             }}
-                            InputLabelProps={{ style: { fontSize: `calc(28/1080 * ${props.bheight})` } }} />
+                            InputLabelProps={{ style: inputValuesStyle }} />
                         <div className="d-flex flex-row align-items-center mb-3" style={{ fontSize: `calc(20/1080 * ${props.bheight})` }}>
                             <Checkbox />
                             <div>I accept the&nbsp;</div>
@@ -58,16 +67,21 @@ function Register(props) {
 
                     </div>
                 </div>
-                <div className="position-absolute" style={{ bottom: 0 }}>Image Here</div>
+                <div className="position-absolute" style={{ bottom: 0 }}>
+                    <img src={img00} style={{ height: `calc(861/1080 * ${props.bheight})`, width: `calc(861/1080 * ${props.bheight} * 536/861)` }} alt="" />
+                </div>
             </div>
         );
     }
 
-    function Mobile(props) {
-        return (
-            <div className={props.className} style={props.style}>
-                <div className="d-flex flex-column" style={{ width: '60vw' }}>
-                    <div className="mb-3 d-flex justify-content-center" style={{ fontSize: `calc(36/1080 * ${props.bheight})`, letterSpacing: `calc(3.6/1080 * ${props.bheight})`, fontWeight: '500' }}>Register</div>
+    function Mobile() {
+        function TopBar() {
+            return (
+                <>
+                    <div className="mb-3 d-flex justify-content-center" style={{
+                        fontSize: `calc(36/1080 * ${props.bheight})`,
+                        letterSpacing: `calc(3.6/1080 * ${props.bheight})`, fontWeight: '500'
+                    }}>Register</div>
                     <TextField className="mb-3" label='Username' size="small" placeholder="Enter Username Here"
                         InputProps={{
                             style: { fontSize: `calc(15px)` },
@@ -99,29 +113,47 @@ function Register(props) {
                             <div className="text-primary">terms &amp; condition</div>
                         </div>
                     </div>
+
                     <div className="d-flex flex-row justify-content-center mb-3">
-                        <Button className="" variant="contained" size="small" sx={{ fontSize: `calc(50/1920 * 100vw)` }}>Register</Button>
+                        <Button className="" variant="contained" size="small" sx={{ fontSize: `calc(18/1080 * ${props.bheight})` }}>Register</Button>
                     </div>
+                    <div style={{ backgroundColor: '#707070', width: '60%', height: '1px' }}></div>
                     <div className="mb-3" style={{ height: '1px', backgroundColor: 'black' }}></div>
                     <div className="mb-3 d-flex justify-content-center" style={{ fontSize: `calc(60/1920 * 100vw)`, letterSpacing: `calc(6/1920 * 100vw)` }}><b>Already have an account</b></div>
                     <div className="mb-1 d-flex flex-column align-items-center" >
                         <div>Or Sign In Using</div>
                         <div><Facebook /><Google /></div>
                     </div>
+                </>
+            );
+        }
+        return (
+            <>
+                <div className='position-relative' style={{ display: 'block', width: '100%', height: 'max-content', backgroundColor: '#FBE551' }}>
+                    <div className="d-flex justify-content-center" style={{ width: '100vw' }}>
+                        <div className='position-relative d-flex flex-column align-items-center' style={{ maxWidth: '500px', width: '80vw' }}>
+                            <TopBar />
+                        </div>
+                    </div>
+                    <div className="position-relative">
+                        <div className='position-absolute d-flex justify-content-center ' style={{ width: '100vw' }}>
+                            <img className='' src={imggetstarted} style={{ height: `calc(200/1080 * ${props.bheight})` }} alt="" />
+                        </div>
+                        <div className='position-relative' style={{ height: `calc(150/640 * ${props.bheight})` }}>
+                            <img className='' src={img00} style={{ height: `calc(200/640 * ${props.bheight})`, transform: `translateY(calc(-50/640 * ${props.bheight}))` }} alt="" />
+                        </div>
+
+                    </div>
                 </div>
-                <div className="" style={{ height: `calc(300/640 * 100vw)`, overflow: 'hidden' }} >
-                    <div className="position-relative" style={{ fontSize: `calc(196/1920 * 100vw)`, fontWeight: '700', color: '#FFF5CC' }}>GET STARTED</div>
-                    <div className="position-relative" style={{ top: `calc(-40/640 * ${props.bheight})`, fontSize: `calc(500/1920 * 100vw)`, fontWeight: '700', color: '#FFF5CC' }}>HERE</div>
-                </div>
-            </div>
-        );
+
+            </>);
     }
     return (
         <>
-            <div>
+            <div style={{ width: '100%', height: '100%' }}>
                 <NavBar position="fixed" brand="GymFit" minHeight='50px' />
 
-                <div className="w-100 d-block position-absolute" style={{ backgroundColor: 'none' }}>
+                <div className="w-100 d-block position-absolute" style={{ width: '100%', height: '100%' }}>
 
                     {/* Desktop Screen */}
                     <Box sx={{ display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex', xl: 'flex' }, width: '100%', height: props.bheight }}>
@@ -131,10 +163,11 @@ function Register(props) {
                     </Box>
 
                     {/* Mobile Screen */}
-                    <Box sx={{ display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none', xl: 'none' }, width: '100%', minHeight: props.bheight, backgroundColor: '#FBE551' }}>
-                        <div className="pt-5">
-                            <Mobile className='flex-grow-1 pt-3 d-flex flex-column align-items-center justify-content-center' bheight={props.bheight} style={{ height: '100%', width: '100vw' }} />
+                    <Box sx={{ display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none', xl: 'none' }, width: '100%', height: '100%', backgroundColor: '#FBE551' }}>
+                        <div className="mt-5 pt-5" style={{ width: '100%' }}>
+                            <Mobile />
                         </div>
+
                     </Box>
                 </div>
 
