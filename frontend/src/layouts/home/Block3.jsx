97861e26c8ae5dfeb42b32431a9fcc2980262d8f
devharp <hsingh.161297@gmail.com>
2022-03-25 09:22:04 +0530

    Fixed the image urls in Block3.jsx

--- frontend/src/layouts/home/Block3.jsx
+++ frontend/src/layouts/home/Block3.jsx
@@ -45,7 +45,7 @@ function Block3(props) {
                             boxShadow: '0px 14px 8px #00000029'
                         }}>
                             <div className='d-flex justify-content-center' style={{ backgroundColor: '#FFF5CC', height: `calc(240/1080 * ${props.bheight})`, width: `calc(240/1080 * ${props.bheight}) * 1.4`, flexShrink: '0' }}>
-                                <img src={`http://localhost:3001${props.content.other[0].img}`} style={{ width: `calc(572/1920 * ${props.bheight})` }} alt="" />
+                                <img src={`${props.content.other[0].img}`} style={{ width: `calc(572/1920 * ${props.bheight})` }} alt="" />
                             </div>
                             <div style={{ textAlign: 'center', textAlignLast: 'center', maxWidth: `calc(260/1080 * ${props.bheight})`, fontSize: `calc(28/1080 * ${props.bheight})`, fontWeight: '500', marginTop: `calc(0.5/100 * ${props.bheight})` }}>{props.content.other[0].text}</div>
 
@@ -57,7 +57,7 @@ function Block3(props) {
                             boxShadow: '0px 14px 8px #00000029'
                         }}>
                             <div className='d-flex justify-content-center' style={{ backgroundColor: '#FFF5CC', height: `calc(240/1080 * ${props.bheight})`, width: `calc(240/1080 * ${props.bheight}) * 1.4`, flexShrink: '0' }}>
-                                <img src={`http://localhost:3001${props.content.other[1].img}`} style={{ width: `calc(572/1920 * ${props.bheight})` }} alt="" />
+                                <img src={`${props.content.other[1].img}`} style={{ width: `calc(572/1920 * ${props.bheight})` }} alt="" />
                             </div>
                             <div style={{ textAlign: 'center', textAlignLast: 'center', maxWidth: `calc(260/1080 * ${props.bheight})`, fontSize: `calc(28/1080 * ${props.bheight})`, fontWeight: '500', marginTop: `calc(0.5/100 * ${props.bheight})` }}>{props.content.other[1].text}</div>
                         </div>
@@ -68,7 +68,7 @@ function Block3(props) {
                             boxShadow: '0px 14px 8px #00000029'
                         }}>
                             <div className='d-flex justify-content-center' style={{ backgroundColor: '#FFF5CC', height: `calc(240/1080 * ${props.bheight})`, width: `calc(240/1080 * ${props.bheight}) * 1.4`, flexShrink: '0' }}>
-                                <img src={`http://localhost:3001${props.content.other[2].img}`} style={{ width: `calc(572/1920 * ${props.bheight})` }} alt="" />
+                                <img src={`${props.content.other[2].img}`} style={{ width: `calc(572/1920 * ${props.bheight})` }} alt="" />
                             </div>
                             <div style={{ textAlign: 'center', textAlignLast: 'center', maxWidth: `calc(260/1080 * ${props.bheight})`, fontSize: `calc(28/1080 * ${props.bheight})`, fontWeight: '500', marginTop: `calc(0.5/100 * ${props.bheight})` }}>{props.content.other[2].text}</div>                            
                         </div>
@@ -109,7 +109,7 @@ function Block3(props) {
                             boxShadow: '0px 14px 8px #00000029'
                         }}>
                             <div className='d-flex justify-content-center' style={{ backgroundColor: '#FFF5CC', height: `calc(67/640 * ${props.bheight})`, width: `calc(67/640 * ${props.bheight} * 1.4)` }}>
-                                <img src={`http://localhost:3001${props.content.other[1].img}`} style={{ width: `calc(70/640 * ${props.bheight})` }} alt="" />
+                                <img src={`${props.content.other[1].img}`} style={{ width: `calc(70/640 * ${props.bheight})` }} alt="" />
                             </div>
                             <div style={{ maxWidth: `calc(30/320 * 100vw)`, textAlign: 'center', textAlignLast: 'center', fontSize: `calc(7/640 * ${props.bheight})`, fontWeight: '500', marginTop: `calc(0.5/100 * ${props.bheight})` }}>{props.content.other[0].text}</div>
 
@@ -121,7 +121,7 @@ function Block3(props) {
                             boxShadow: '0px 14px 8px #00000029'
                         }}>
                             <div className='d-flex justify-content-center' style={{ backgroundColor: '#FFF5CC', height: `calc(67/640 * ${props.bheight})`, width: `calc(67/640 * ${props.bheight} * 1.4)` }}>
-                                <img src={img31} style={{ width: `calc(70/640 * ${props.bheight})` }} alt="" />
+                                <img src={`${props.content.other[1].img}`} style={{ width: `calc(70/640 * ${props.bheight})` }} alt="" />
                             </div>
                             <div style={{ maxWidth: `calc(30/320 * 100vw)`, textAlign: 'center', textAlignLast: 'center', fontSize: `calc(7/640 * ${props.bheight})`, fontWeight: '500', marginTop: `calc(0.5/100 * ${props.bheight})` }}>{props.content.other[1].text}</div>
                             
@@ -133,7 +133,7 @@ function Block3(props) {
                             boxShadow: '0px 14px 8px #00000029'
                         }}>
                             <div className='d-flex justify-content-center' style={{ backgroundColor: '#FFF5CC', height: `calc(67/640 * ${props.bheight})`, width: `calc(67/640 * ${props.bheight} * 1.4)` }}>
-                                <img src={img32} style={{ width: `calc(70/640 * ${props.bheight})` }} alt="" />
+                                <img src={`${props.content.other[2].img}`} style={{ width: `calc(70/640 * ${props.bheight})` }} alt="" />
                             </div>
                             <div style={{ maxWidth: `calc(30/320 * 100vw)`, textAlign: 'center', textAlignLast: 'center', fontSize: `calc(7/640 * ${props.bheight})`, fontWeight: '500', marginTop: `calc(0.5/100 * ${props.bheight})` }}>{props.content.other[2].text}</div>
 
