8f7512f333c7c8b74220544183fefbc99a054699
devharp <hsingh.161297@gmail.com>
2022-04-04 22:39:29 +0530

    Added a new 'pending-list'

--- frontend/src/layouts/home/Block1.jsx
+++ frontend/src/layouts/home/Block1.jsx
@@ -38,7 +38,7 @@ function Block1(props) {
                 <Box sx={{ display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex', xl: 'flex' }, height: '100%' }}>
                     <div className='mt-5 pt-3' style={{ height: '100%', width: '100%', zIndex: '1' }}>
                         <div className="d-flex flex-column justify-content-end align-items-center w-100" style={{ overflow: 'hidden' }}>
-                            <div className="mb-4" style={{ fontSize: `calc(0.045 * ${props.bheight})`, letterSpacing: '0.75vw' }}>{props.content.heading}</div>
+                            <div className="mb-4 mt-5" style={{ fontSize: `calc(0.045 * ${props.bheight})`, letterSpacing: '0.75vw' }}>{props.content.heading}</div>
                             <div className="mb-4" style={{ backgroundColor: '#04C7F4', width: '30vw', height: '2px' }}></div>
                             <div className="mb-4 d-flex flex-column align-items-center" style={{ maxWidth: `calc(1000/1920 * 100vw)`, fontSize: `calc(28/1920 * 100vw)`, textAlign: 'center', textAlignLast: 'center' }}>
                                 {props.content.paragraph}
@@ -46,7 +46,7 @@ function Block1(props) {
                             <div className="mb-4" style={{ fontSize: `calc(0.03 * ${props.bheight})`, letterSpacing: '0.16vw' }}>{props.content.other}</div>
                             <div className="d-block" style={{ height: '45%', width: '100vw', backgroundColor: 'none' }}>
                                 <div className="d-flex justify-content-center">
-                                    <img style={{ height: `calc(0.45 * ${props.bheight})` }} src={img10} />
+                                    <img style={{ height: `calc(0.5 * ${props.bheight})` }} src={img10} />
                                 </div>
                                 <div className='position-absolute d-flex justify-content-center' style={{ transform: `translateY(calc(-110/1080 * ${props.bheight}))`, width: '100%' }}>
                                     <Button className='px-3' size='small' sx={{ '&:hover' : { backgroundColor: 'rgb(4, 199, 244)' }, backgroundColor: 'rgb(4, 199, 244)', fontSize: `calc(20/1080 * ${props.bheight})`, borderRadius: `calc(25/1080 * ${props.bheight})` }} variant='contained'>Enroll Now</Button>
